36077d932fecd0b64f56c188e1177ee6471c5f41
vincentmilliken <vincentamilliken@gmail.com>
2012-11-30 11:15:30 +0000

    Removed secret and key

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'YHDsOgEhMH72oeR56eQOxw');
-define('CONSUMER_SECRET', '379Uv0rX4JGC3l2ZLkQ1EtsE0Hw52OlZvzMfpoDg');
-define('OAUTH_CALLBACK', 'http://labs.vincentmilliken.com/twitterprofile/callback.php');
+define('CONSUMER_KEY', ''); 
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', '');
